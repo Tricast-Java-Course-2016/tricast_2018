c4a1f2834095fdc101aa755f43b1c051a428b86f
Tamas Balogh <tamas.balogh.ext@lvs.co.uk>
2018-10-03 14:32:34 +0200

    Committing database generator scripts

---
+++ source/main/resources/data_generator_script.sql
@@ -0,0 +1,133 @@
+INSERT INTO "tricast"."sports" ("id", "description") VALUES
+	(1, 'Football'),
+	(2, 'Horse Racing');
+
+INSERT INTO "tricast"."competitors" ("id", "description") VALUES
+	(1, 'FTC'),
+	(2, 'ETO'),
+	(3, 'Arsenal'),
+	(4, 'Milan'),
+	(5, 'Real Madrid'),
+	(6, 'Lord Clenaghcastle'),
+	(7, 'Poetic Force'),
+	(8, 'Mamillius'),
+	(9, 'Dourado'),
+	(10, 'Zeyzoun'),
+	(11, 'Walk On Walter'),
+	(12, 'De Vegas Kid'),
+	(13, 'Glenn Coco'),
+	(14, 'Suzis Connoisseur');
+
+INSERT INTO "tricast"."eventtypes" ("id", "description") VALUES
+	(1, 'GAMEEVENT'),
+	(2, 'RANKEVENT');
+
+INSERT INTO "tricast"."periodtypes" ("id", "description") VALUES
+	(1, '90 minutes'),
+	(2, 'First half'),
+	(3, 'Second half'),
+	(4, 'Rac');
+	
+INSERT INTO "tricast"."bettypes"("id", "description") VALUES 
+	(1, 'Single'),
+	(2, 'Double'),
+	(3, 'Treble');
+	
+INSERT INTO "tricast"."markettypes" ("id", "description", "sportid") VALUES
+	(1, 'WDW', 1),
+	(2, 'Correct score', 1),
+	(3, 'Over/Under Goals 1.5', 1),
+	(4, 'Over/Under Goals 2.5', 1),
+	(5, 'Over/Under Corners 8.5', 1),
+	(6, 'Half time / Full time', 1),
+	(7, 'Outright', 1),
+	(8, 'Head to head', 1),
+	(9, 'Outright', 2),
+	(10, 'Head to head', 2);
+
+INSERT INTO "tricast"."resulttypes" ("id", "description") VALUES
+	(1, 'Goals'),
+	(2, 'Corners'),
+	(3, 'Positions');
+	
+INSERT INTO "tricast"."accounts"("id", "username", "password", "firstname", "lastname", "dob", "address", "emailaddress", "phonenumber", "pin", "bankaccountnumber", "bankcardnumber", "createddate", "accounttype") VALUES 
+	(1, 'Ferike12', 'password123', 'Ferenc', 'Nagy', '19890216', '9012, Győr Hegyalja u. 116','ferike12@gmail.com', '+36302658945', '234523AA', '1234567879985421', '5454545454848484', current_timestamp,'PLAYER'),
+	(2, 'BlackAdder', 'password123', 'Edmund', 'Blackadder', '19750312', '9999, Edinburgh Edinburgh Castl','blackadder@gmail.com', '+36202354955', '443521A', '1413564879975421', '545466774848484', current_timestamp,'PLAYER'),
+	(3, 'RunningMan', 'password123', 'Ben', 'Richards', '19810119', '1111, Co-op City, Cullum Street 12','benrichards12@gmail.com', '+36702458945', '666677EA', '1234467879995929', '6654545454848484', current_timestamp,'PLAYER'),
+	(4, 'admin', 'admin', 'Adminus', 'Admin', '00000101', '0000, ?','deity@gmail.com', '-', '-', '-', '-', current_timestamp,'ADMIN');
+	
+	
+INSERT INTO "tricast"."leagues" ("id", "description", "sportid") VALUES
+	(1, 'EB 2018', 1),
+	(2, 'EB 2017', 1),
+	(3, 'VB 2018', 1),
+	(4, 'VB 2017', 1),
+	(5, 'EPSOM DOWNS 2018-08-17', 2),
+	(6, 'MUSSELBURGH 2018-11-01', 2);
+
+INSERT INTO "tricast"."leaguecompetitormap" ("competitorid", "leagueid") VALUES
+	(1, 1),
+	(3, 1),
+	(5, 1),
+	(10, 5),
+	(11, 5),
+	(13, 5);
+
+INSERT INTO "tricast"."events" ("id", "eventtypeid", "leagueid", "description", "status", "starttime") VALUES
+	(1, 1, 1, E'Szuper EB döntő', E'OPEN', E'2018-09-30 17:45:22+02'),
+	(2, 2, 5, E'A nagy lóverseny', E'OPEN', E'2018-09-30 17:45:35+02'),
+	(3, 1, 1, E'EB elődöntő', E'OPEN', E'2018-09-30 17:45:52+02');
+
+INSERT INTO "tricast"."eventcompetitormap" ("id", "competitorid", "eventid") VALUES
+	(1, 1, 1),
+	(2, 3, 1),
+	(3, 6, 2),
+	(4, 7, 2),
+	(5, 8, 2),
+	(6, 9, 2),
+	(7, 10, 2),
+	(8, 11, 2),
+	(9, 12, 2),
+	(10, 13, 2),
+	(11, 14, 2),
+	(12, 2, 3),
+	(13, 4, 3);
+	
+INSERT INTO "tricast"."markets" ("id", "eventid", "markettypeid", "description", "periodtypeid") VALUES
+	(1, 1, 4, E'Szuper EB döntő - FTC - ARSENAL - 2018.09.30. 18:30 - 1x2', 1),
+	(2, 3, 3, E'EB elődöntő - ETO - MILAN - 2018.09.30. 18:30 - 1x2', 2),
+	(3, 2, 8, E'A nagy lóverseny - Lord Clenaghcastle - Poetic Force', 4),
+	(4, 2, 8, E'A nagy lóverseny - Mamillius - Poetic Force', 4);
+	
+INSERT INTO tricast.outcomes(id, marketid, outcomecode, description, odds) VALUES 
+	(1, 1, '1', 'FTC', 3.10),
+	(2, 1, 'X', 'Döntetlen', 2.10),
+	(3, 1, '2', 'ARSENAL', 1.15),
+	(4, 2, '1', 'ETO', 4),
+	(5, 2, 'X', 'Döntetlen', 2.15),
+	(6, 2, '2', 'MILAN', 1.2),
+	(7, 3, '1', 'Lord Clenaghcastle', 1.4),
+	(8, 3, '2', 'Poetic Force', 3.15),
+	(9, 4, '1', 'Mamillius', 1.15),
+	(10, 4, '2', 'Poetic Force', 2.15);
+	
+INSERT INTO tricast.results(id, resulttypeid, result, eventcompetitormapid, periodtypeid) VALUES 
+	(1, 1, 1, 1, 1),
+	(2, 1, 0, 2, 1),
+	(3, 3, 2, 3, 4),
+	(4, 3, 4, 4, 4);
+	
+INSERT INTO tricast.bets(id, bettypeid, accountid) VALUES 
+	(1, 2, 1),
+	(2, 1, 2);
+	
+INSERT INTO tricast.betoutcomemap(betid, outcomeid, odds) VALUES 
+	(1, 1, 3.10),
+	(1, 4, 4),
+	(2, 8, 3.15);
+	
+INSERT INTO tricast.transactions(id, betid, createddate, description, amount, accountid, type) VALUES 
+	(1, null, CURRENT_TIMESTAMP, 'Bankkártya feltöltés', 1000, 1, 'TRANSFER'),
+	(2, 1, CURRENT_TIMESTAMP, 'Double Bet FTC @ 3.10, ETO @ 4.00', -10, 1, 'BET'),
+	(3, null, CURRENT_TIMESTAMP, 'Bankkártya feltöltés', 500, 2, 'TRANSFER'),
+	(4, 2, CURRENT_TIMESTAMP, 'Single Bet Poetic Force @ 3.15', -20, 2, 'BET');
---
+++ source/main/resources/database_generator_script.sql
@@ -0,0 +1,491 @@
+SET statement_timeout = 0;
+SET lock_timeout = 0;
+SET idle_in_transaction_session_timeout = 0;
+SET client_encoding = 'UTF8';
+SET standard_conforming_strings = on;
+SELECT pg_catalog.set_config('search_path', '', false);
+SET check_function_bodies = false;
+SET client_min_messages = warning;
+SET row_security = off;
+DROP SCHEMA IF EXISTS tricast CASCADE;
+CREATE SCHEMA tricast;
+ALTER SCHEMA tricast OWNER TO postgres;
+
+SET default_tablespace = '';
+
+SET default_with_oids = false;
+
+CREATE TABLE tricast.accounts (
+    id integer NOT NULL,
+    username character varying(30) NOT NULL,
+    password character varying(50) NOT NULL,
+    firstname character varying(50) NOT NULL,
+    lastname character varying(50) NOT NULL,
+    dob character varying(8) NOT NULL,
+    address character varying(300) NOT NULL,
+    emailaddress character varying(100),
+    phonenumber character varying(50),
+    pin character varying(20),
+    bankaccountnumber character varying(40),
+    bankcardnumber character varying(40),
+    createddate timestamp(6) with time zone NOT NULL,
+    accounttype character varying(8) NOT NULL
+);
+
+CREATE SEQUENCE tricast.accounts_id_seq
+    AS integer
+    START WITH 100
+    INCREMENT BY 1
+    NO MINVALUE
+    NO MAXVALUE
+    CACHE 1;
+	
+ALTER SEQUENCE tricast.accounts_id_seq OWNED BY tricast.accounts.id;
+
+CREATE TABLE tricast.betoutcomemap (
+    betid integer NOT NULL,
+    outcomeid integer NOT NULL,
+    odds numeric(18,2) NOT NULL
+);
+
+CREATE TABLE tricast.bets (
+    id integer NOT NULL,
+    bettypeid integer NOT NULL,
+    accountid integer NOT NULL
+);
+
+CREATE SEQUENCE tricast.bets_id_seq
+    AS integer
+    START WITH 100
+    INCREMENT BY 1
+    NO MINVALUE
+    NO MAXVALUE
+    CACHE 1;
+	
+ALTER SEQUENCE tricast.bets_id_seq OWNED BY tricast.bets.id;
+
+CREATE TABLE tricast.bettypes (
+    id integer NOT NULL,
+    description character varying(50) NOT NULL
+);
+
+CREATE SEQUENCE tricast.bettypes_id_seq
+    AS integer
+    START WITH 100
+    INCREMENT BY 1
+    NO MINVALUE
+    NO MAXVALUE
+    CACHE 1;
+	
+ALTER SEQUENCE tricast.bettypes_id_seq OWNED BY tricast.bettypes.id;
+
+CREATE TABLE tricast.competitors (
+    id integer NOT NULL,
+    description character varying(50) NOT NULL
+);
+
+CREATE SEQUENCE tricast.competitors_id_seq
+    AS integer
+    START WITH 100
+    INCREMENT BY 1
+    NO MINVALUE
+    NO MAXVALUE
+    CACHE 1;
+	
+ALTER SEQUENCE tricast.competitors_id_seq OWNED BY tricast.competitors.id;
+
+CREATE TABLE tricast.eventcompetitormap (
+    id bigint NOT NULL,
+    competitorid integer NOT NULL,
+    eventid integer NOT NULL
+);
+
+CREATE SEQUENCE tricast.eventcompetitormap_id_seq
+    AS bigint
+    START WITH 100
+    INCREMENT BY 1
+    NO MINVALUE
+    NO MAXVALUE
+    CACHE 1;
+	
+ALTER SEQUENCE tricast.eventcompetitormap_id_seq OWNED BY tricast.eventcompetitormap.id;
+
+CREATE TABLE tricast.events (
+    id integer NOT NULL,
+    eventtypeid integer NOT NULL,
+    leagueid integer NOT NULL,
+    description character varying(100) NOT NULL,
+    status character varying(30) NOT NULL,
+    starttime timestamp(6) with time zone
+);
+
+CREATE SEQUENCE tricast.events_id_seq
+    AS integer
+    START WITH 100
+    INCREMENT BY 1
+    NO MINVALUE
+    NO MAXVALUE
+    CACHE 1;
+	
+ALTER SEQUENCE tricast.events_id_seq OWNED BY tricast.events.id;
+
+CREATE TABLE tricast.eventtypes (
+    id integer NOT NULL,
+    description character varying(50) NOT NULL
+);
+
+CREATE SEQUENCE tricast.eventtypes_id_seq
+    AS integer
+    START WITH 100
+    INCREMENT BY 1
+    NO MINVALUE
+    NO MAXVALUE
+    CACHE 1;
+	
+ALTER SEQUENCE tricast.eventtypes_id_seq OWNED BY tricast.eventtypes.id;
+
+CREATE TABLE tricast.leaguecompetitormap (
+    competitorid integer NOT NULL,
+    leagueid integer NOT NULL
+);
+
+CREATE TABLE tricast.leagues (
+    id integer NOT NULL,
+    description character varying(50) NOT NULL,
+    sportid integer NOT NULL
+);
+
+CREATE SEQUENCE tricast.leagues_id_seq
+    AS integer
+    START WITH 100
+    INCREMENT BY 1
+    NO MINVALUE
+    NO MAXVALUE
+    CACHE 1;
+	
+ALTER SEQUENCE tricast.leagues_id_seq OWNED BY tricast.leagues.id;
+
+CREATE TABLE tricast.markets (
+    id integer NOT NULL,
+    eventid integer NOT NULL,
+    markettypeid integer NOT NULL,
+    description character varying(200) NOT NULL,
+    periodtypeid integer NOT NULL
+);
+
+CREATE SEQUENCE tricast.markets_id_seq
+    AS integer
+    START WITH 100
+    INCREMENT BY 1
+    NO MINVALUE
+    NO MAXVALUE
+    CACHE 1;
+	
+ALTER SEQUENCE tricast.markets_id_seq OWNED BY tricast.markets.id;
+
+CREATE TABLE tricast.markettypes (
+    id integer NOT NULL,
+    description character varying(50) NOT NULL,
+    sportid integer NOT NULL
+);
+
+CREATE SEQUENCE tricast.markettypes_id_seq
+    AS integer
+    START WITH 100
+    INCREMENT BY 1
+    NO MINVALUE
+    NO MAXVALUE
+    CACHE 1;
+	
+ALTER SEQUENCE tricast.markettypes_id_seq OWNED BY tricast.markettypes.id;
+
+CREATE TABLE tricast.outcomes (
+    id integer NOT NULL,
+    marketid integer NOT NULL,
+    outcomecode character varying(5) NOT NULL,
+    description character varying(50) NOT NULL,
+    odds numeric(18,2),
+    winyn numeric(1,0)
+);
+
+CREATE SEQUENCE tricast.outcomes_id_seq
+    AS integer
+    START WITH 100
+    INCREMENT BY 1
+    NO MINVALUE
+    NO MAXVALUE
+    CACHE 1;
+	
+ALTER SEQUENCE tricast.outcomes_id_seq OWNED BY tricast.outcomes.id;
+
+CREATE TABLE tricast.periodtypes (
+    id integer NOT NULL,
+    description character varying(50) NOT NULL
+);
+
+CREATE SEQUENCE tricast.periodtypes_id_seq
+    AS integer
+    START WITH 100
+    INCREMENT BY 1
+    NO MINVALUE
+    NO MAXVALUE
+    CACHE 1;
+	
+ALTER SEQUENCE tricast.periodtypes_id_seq OWNED BY tricast.periodtypes.id;
+
+CREATE TABLE tricast.results (
+    id integer NOT NULL,
+    resulttypeid integer NOT NULL,
+    result numeric(18,0) NOT NULL,
+    periodtypeid integer NOT NULL,
+	eventcompetitormapid bigint 
+);
+
+CREATE SEQUENCE tricast.results_id_seq
+    AS integer
+    START WITH 100
+    INCREMENT BY 1
+    NO MINVALUE
+    NO MAXVALUE
+    CACHE 1;
+	
+ALTER SEQUENCE tricast.results_id_seq OWNED BY tricast.results.id;
+
+CREATE TABLE tricast.resulttypes (
+    id integer NOT NULL,
+    description character varying(50) NOT NULL
+);
+
+CREATE SEQUENCE tricast.resulttypes_id_seq
+    AS integer
+    START WITH 100
+    INCREMENT BY 1
+    NO MINVALUE
+    NO MAXVALUE
+    CACHE 1;
+	
+ALTER SEQUENCE tricast.resulttypes_id_seq OWNED BY tricast.resulttypes.id;
+
+CREATE TABLE tricast.transactions (
+    id integer NOT NULL,
+    betid integer,
+    createddate timestamp(6) with time zone NOT NULL,
+    description character varying(60) NOT NULL,
+    amount numeric(18,2) NOT NULL,
+    accountid integer NOT NULL,
+    type character varying(13) NOT NULL
+);
+
+CREATE SEQUENCE tricast.transactions_id_seq
+    AS integer
+    START WITH 100
+    INCREMENT BY 1
+    NO MINVALUE
+    NO MAXVALUE
+    CACHE 1;
+	
+ALTER SEQUENCE tricast.transactions_id_seq OWNED BY tricast.transactions.id;
+
+CREATE TABLE tricast.sports (
+    id integer NOT NULL,
+    description character varying(50) NOT NULL
+);
+
+CREATE SEQUENCE tricast.sports_id_seq
+    AS integer
+    START WITH 100
+    INCREMENT BY 1
+    NO MINVALUE
+    NO MAXVALUE
+    CACHE 1;
+	
+ALTER SEQUENCE tricast.sports_id_seq OWNED BY tricast.transactions.id;
+
+ALTER TABLE ONLY tricast.accounts ALTER COLUMN id SET DEFAULT nextval('tricast.accounts_id_seq'::regclass);
+
+ALTER TABLE ONLY tricast.bets ALTER COLUMN id SET DEFAULT nextval('tricast.bets_id_seq'::regclass);
+
+ALTER TABLE ONLY tricast.bettypes ALTER COLUMN id SET DEFAULT nextval('tricast.bettypes_id_seq'::regclass);
+
+ALTER TABLE ONLY tricast.competitors ALTER COLUMN id SET DEFAULT nextval('tricast.competitors_id_seq'::regclass);
+
+ALTER TABLE ONLY tricast.events ALTER COLUMN id SET DEFAULT nextval('tricast.events_id_seq'::regclass);
+
+ALTER TABLE ONLY tricast.eventtypes ALTER COLUMN id SET DEFAULT nextval('tricast.eventtypes_id_seq'::regclass);
+
+ALTER TABLE ONLY tricast.leagues ALTER COLUMN id SET DEFAULT nextval('tricast.leagues_id_seq'::regclass);
+
+ALTER TABLE ONLY tricast.markets ALTER COLUMN id SET DEFAULT nextval('tricast.markets_id_seq'::regclass);
+
+ALTER TABLE ONLY tricast.markettypes ALTER COLUMN id SET DEFAULT nextval('tricast.markettypes_id_seq'::regclass);
+
+ALTER TABLE ONLY tricast.outcomes ALTER COLUMN id SET DEFAULT nextval('tricast.outcomes_id_seq'::regclass);
+
+ALTER TABLE ONLY tricast.periodtypes ALTER COLUMN id SET DEFAULT nextval('tricast.periodtypes_id_seq'::regclass);
+
+ALTER TABLE ONLY tricast.results ALTER COLUMN id SET DEFAULT nextval('tricast.results_id_seq'::regclass);
+
+ALTER TABLE ONLY tricast.resulttypes ALTER COLUMN id SET DEFAULT nextval('tricast.resulttypes_id_seq'::regclass);
+
+ALTER TABLE ONLY tricast.transactions ALTER COLUMN id SET DEFAULT nextval('tricast.transactions_id_seq'::regclass);
+
+ALTER TABLE ONLY tricast.sports ALTER COLUMN id SET DEFAULT nextval('tricast.sports_id_seq'::regclass);
+
+ALTER TABLE ONLY tricast.accounts
+    ADD CONSTRAINT accounts_pkey PRIMARY KEY (id);
+
+ALTER TABLE ONLY tricast.betoutcomemap
+    ADD CONSTRAINT betoutcomemap_pkey PRIMARY KEY (betid, outcomeid);
+
+ALTER TABLE ONLY tricast.bets
+    ADD CONSTRAINT bets_pkey PRIMARY KEY (id);
+
+ALTER TABLE ONLY tricast.bettypes
+    ADD CONSTRAINT bettypes_pkey PRIMARY KEY (id);
+
+ALTER TABLE ONLY tricast.competitors
+    ADD CONSTRAINT competitors_pkey PRIMARY KEY (id);
+
+ALTER TABLE ONLY tricast.eventcompetitormap
+    ADD CONSTRAINT eventcompetitormap_pkey PRIMARY KEY (id);
+	
+ALTER TABLE ONLY tricast.events
+    ADD CONSTRAINT events_pkey PRIMARY KEY (id);
+
+ALTER TABLE ONLY tricast.eventtypes
+    ADD CONSTRAINT eventtypes_pkey PRIMARY KEY (id);
+	
+ALTER TABLE ONLY tricast.leaguecompetitormap
+    ADD CONSTRAINT leaguecompetitormap_pkey PRIMARY KEY (leagueid, competitorid);
+
+ALTER TABLE ONLY tricast.leagues
+    ADD CONSTRAINT leagues_pkey PRIMARY KEY (id);
+
+ALTER TABLE ONLY tricast.markets
+    ADD CONSTRAINT markets_pkey PRIMARY KEY (id);
+
+ALTER TABLE ONLY tricast.markettypes
+    ADD CONSTRAINT markettypes_pkey PRIMARY KEY (id);
+
+ALTER TABLE ONLY tricast.outcomes
+    ADD CONSTRAINT outcomes_pkey PRIMARY KEY (id);
+
+ALTER TABLE ONLY tricast.periodtypes
+    ADD CONSTRAINT periodtypes_pkey PRIMARY KEY (id);
+
+ALTER TABLE ONLY tricast.results
+    ADD CONSTRAINT results_pkey PRIMARY KEY (id);
+
+ALTER TABLE ONLY tricast.resulttypes
+    ADD CONSTRAINT resulttypes_pkey PRIMARY KEY (id);
+
+ALTER TABLE ONLY tricast.transactions
+    ADD CONSTRAINT transactions_pkey PRIMARY KEY (id);
+	
+ALTER TABLE ONLY tricast.sports
+    ADD CONSTRAINT sports_pkey PRIMARY KEY (id);
+
+CREATE INDEX fki_fk_betoutcomemap_bets ON tricast.betoutcomemap USING btree (betid);
+
+CREATE INDEX fki_fk_betoutcomemap_outcomes ON tricast.betoutcomemap USING btree (outcomeid);
+
+CREATE INDEX fki_fk_bets_accounts ON tricast.bets USING btree (accountid);
+
+CREATE INDEX fki_fk_bets_bettypes ON tricast.bets USING btree (bettypeid);
+
+CREATE INDEX fki_fk_eventcompetitormap_competitors ON tricast.eventcompetitormap USING btree (competitorid);
+
+CREATE INDEX fki_fk_eventcompetitormap_events ON tricast.eventcompetitormap USING btree (eventid);
+
+CREATE INDEX fki_fk_leaguecompetitormap_competitors ON tricast.leaguecompetitormap USING btree (competitorid);
+
+CREATE INDEX fki_fk_leaguecompetitormap_leagues ON tricast.leaguecompetitormap USING btree (leagueid);
+
+CREATE INDEX fki_fk_leagues_sports ON tricast.leagues USING btree (sportid);
+
+CREATE INDEX fki_fk_markets_events ON tricast.markets USING btree (eventid);
+
+CREATE INDEX fki_fk_markets_markettypes ON tricast.markets USING btree (markettypeid);
+
+CREATE INDEX fki_fk_markets_periodtypes ON tricast.markets USING btree (periodtypeid);
+
+CREATE INDEX fki_fk_markettypes_sports ON tricast.markettypes USING btree (sportid);
+
+CREATE INDEX fki_fk_outcomes_markets ON tricast.outcomes USING btree (marketid);
+
+CREATE INDEX fki_fk_events_eventtypes ON tricast.events USING btree (eventtypeid);
+
+CREATE INDEX fki_fk_events_leagues ON tricast.events USING btree (leagueid);
+
+CREATE INDEX fki_fk_results_eventcompetitormap ON tricast.results USING btree (eventcompetitormapid);
+
+CREATE INDEX fki_fk_results_periodtypes ON tricast.results USING btree (periodtypeid);
+
+CREATE INDEX fki_fk_results_resulttypes ON tricast.results USING btree (resulttypeid);
+
+CREATE INDEX fki_fk_transactions_accounts ON tricast.transactions USING btree (accountid);
+
+CREATE INDEX fki_fk_transactions_bets ON tricast.transactions USING btree (betid);
+
+ALTER TABLE ONLY tricast.betoutcomemap
+    ADD CONSTRAINT fk_betoutcomemap_bets FOREIGN KEY (betid) REFERENCES tricast.bets(id);
+
+ALTER TABLE ONLY tricast.betoutcomemap
+    ADD CONSTRAINT fk_betoutcomemap_outcomes FOREIGN KEY (outcomeid) REFERENCES tricast.outcomes(id);
+
+ALTER TABLE ONLY tricast.bets
+    ADD CONSTRAINT fk_bets_accounts FOREIGN KEY (accountid) REFERENCES tricast.accounts(id);
+
+ALTER TABLE ONLY tricast.bets
+    ADD CONSTRAINT fk_bets_bettypes FOREIGN KEY (bettypeid) REFERENCES tricast.bettypes(id);
+
+ALTER TABLE ONLY tricast.eventcompetitormap
+    ADD CONSTRAINT fk_eventcompetitormap_competitors FOREIGN KEY (competitorid) REFERENCES tricast.competitors(id);
+
+ALTER TABLE ONLY tricast.eventcompetitormap
+    ADD CONSTRAINT fk_eventcompetitormap_events FOREIGN KEY (eventid) REFERENCES tricast.events(id);
+
+ALTER TABLE ONLY tricast.events
+    ADD CONSTRAINT fk_events_eventtypes FOREIGN KEY (eventtypeid) REFERENCES tricast.eventtypes(id);
+
+ALTER TABLE ONLY tricast.events
+    ADD CONSTRAINT fk_events_leagues FOREIGN KEY (leagueid) REFERENCES tricast.leagues(id);
+
+ALTER TABLE ONLY tricast.leaguecompetitormap
+    ADD CONSTRAINT fk_leaguecompetitormap_competitors FOREIGN KEY (competitorid) REFERENCES tricast.competitors(id);
+
+ALTER TABLE ONLY tricast.leaguecompetitormap
+    ADD CONSTRAINT fk_leaguecompetitormap_leagues FOREIGN KEY (leagueid) REFERENCES tricast.leagues(id);
+
+ALTER TABLE ONLY tricast.leagues
+    ADD CONSTRAINT fk_leagues_sports FOREIGN KEY (sportid) REFERENCES tricast.sports(id);
+	
+ALTER TABLE ONLY tricast.markets
+    ADD CONSTRAINT fk_markets_events FOREIGN KEY (eventid) REFERENCES tricast.events(id);
+
+ALTER TABLE ONLY tricast.markets
+    ADD CONSTRAINT fk_markets_markettypes FOREIGN KEY (markettypeid) REFERENCES tricast.markettypes(id);
+
+ALTER TABLE ONLY tricast.markets
+    ADD CONSTRAINT fk_markets_periodtypes FOREIGN KEY (periodtypeid) REFERENCES tricast.periodtypes(id);
+	
+ALTER TABLE ONLY tricast.markettypes
+    ADD CONSTRAINT fk_markettypes_sports FOREIGN KEY (sportid) REFERENCES tricast.sports(id);
+
+ALTER TABLE ONLY tricast.outcomes
+    ADD CONSTRAINT fk_outcomes_markets FOREIGN KEY (marketid) REFERENCES tricast.markets(id);
+
+ALTER TABLE ONLY tricast.results
+    ADD CONSTRAINT fk_results_eventcompetitormap FOREIGN KEY (eventcompetitormapid) REFERENCES tricast.eventcompetitormap(id);
+	
+ALTER TABLE ONLY tricast.results
+    ADD CONSTRAINT fk_results_periodtypes FOREIGN KEY (periodtypeid) REFERENCES tricast.periodtypes(id);
+
+ALTER TABLE ONLY tricast.results
+    ADD CONSTRAINT fk_results_resulttypes FOREIGN KEY (resulttypeid) REFERENCES tricast.resulttypes(id);
+
+ALTER TABLE ONLY tricast.transactions
+    ADD CONSTRAINT fk_transactions_accounts FOREIGN KEY (accountid) REFERENCES tricast.accounts(id);
+
+ALTER TABLE ONLY tricast.transactions
+    ADD CONSTRAINT fk_transactions_bets FOREIGN KEY (betid) REFERENCES tricast.bets(id);
