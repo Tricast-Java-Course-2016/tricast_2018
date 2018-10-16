d2665d5fb41124d2cb853feec52be2f6b67a92aa
Akos Toth Faber <akos.toth.faber@tricast.hu>
2018-10-16 16:15:36 +0200

    SB-27 add swagger to sportsbook application

--- src/main/java/com/tricast/SportsbookApplication.java
+++ src/main/java/com/tricast/SportsbookApplication.java
@@ -6,14 +6,29 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 
+import springfox.documentation.builders.PathSelectors;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
+@EnableSwagger2
 @SpringBootApplication
 public class SportsbookApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(SportsbookApplication.class, args);
 	}
-	
-	public @Bean PasswordEncoder passwordEncoder() {
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
 		return new BCryptPasswordEncoder();
 	}
+
+    @Bean
+    public Docket api() {
+        return new Docket(DocumentationType.SWAGGER_2).select()
+                .apis(RequestHandlerSelectors.basePackage("com.tricast.controllers"))
+                .paths(PathSelectors.any()).build();
+    }
 }
