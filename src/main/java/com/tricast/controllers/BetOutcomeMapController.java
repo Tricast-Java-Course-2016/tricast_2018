5ee3139e6954f1fdcf9d4bb60d93d0c4a92fb4c8
dunaroliver <d.oliver95@hotmail.com>
2018-10-18 16:40:49 +0200

    SB-13 Removed unwanted methods from controller

--- src/main/java/com/tricast/controllers/BetOutcomeMapController.java
+++ src/main/java/com/tricast/controllers/BetOutcomeMapController.java
@@ -2,13 +2,9 @@ package com.tricast.controllers;
 
 import java.util.List;
 
-import javax.inject.Inject;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -22,20 +18,10 @@ public class BetOutcomeMapController {
     @Autowired
     private BetOutcomeMapManager betoutcomemapManager;
 
-    @Inject
-    public BetOutcomeMapController(BetOutcomeMapManager boMapManager) {
-        this.betoutcomemapManager = boMapManager;
-    }
-
     @GetMapping
     public List<BetOutcomeMap> findAll() {
         return betoutcomemapManager.findAll();
     }
-
-    @PostMapping
-    public BetOutcomeMap create(@RequestBody BetOutcomeMap betoutcomemap) {
-        return betoutcomemapManager.create(betoutcomemap);
-    }    
 
     @GetMapping(path = "findById/{id}")
     public BetOutcomeMap findById(@PathVariable("id") Long id) {
