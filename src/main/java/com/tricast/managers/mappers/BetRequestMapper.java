c1735a53a1372de6a17869903060da7ed6a8055b
dunaroliver <d.oliver95@hotmail.com>
2018-11-04 12:05:36 +0100

    SB-32 Fixed Transaction description, preparation for combinations

--- src/main/java/com/tricast/managers/mappers/BetRequestMapper.java
+++ src/main/java/com/tricast/managers/mappers/BetRequestMapper.java
@@ -21,6 +21,7 @@ import com.tricast.repositories.OutcomeRepository;
 import com.tricast.repositories.TransactionRepository;
 import com.tricast.repositories.entities.Bet;
 import com.tricast.repositories.entities.BetOutcomeMap;
+import com.tricast.repositories.entities.BetTypes;
 import com.tricast.repositories.entities.Transaction;
 import com.tricast.repositories.entities.TransactionTypes;
 
@@ -40,54 +41,64 @@ public class BetRequestMapper {
             return null;
         }
         
-        BetPlacementResponse betPlacementResponse = new BetPlacementResponse();
-        betPlacementResponse.setSumStake(BigDecimal.valueOf(0.));
-        betPlacementResponse.setSumPotentialWin(BigDecimal.valueOf(0.));
-        
-        List <BetResponse> betResponses=new ArrayList <BetResponse>();
-        int NumberOfOutcomes=requestObject.getOutcomeOdds().size();
-        List <Bet> bets=new ArrayList <Bet>();
-        List <Transaction> transactions=new ArrayList <Transaction>();
-        List <BetOutcomeMap> betOutcomeMap=new ArrayList <BetOutcomeMap>();
-        Set <Long> outcomeIds=requestObject.getOutcomeOdds().keySet(); 
-    	Long currentId;
-        
-    	Iterator <Long> iterator=outcomeIds.iterator();
-        for(int i=0;i<NumberOfOutcomes;i++) {
-        	currentId=iterator.next();
-        	bets.add(new Bet());
-        	bets.get(i).setAccountId(accountRepository.findById(requestObject.getAccountId()));
-        	bets.get(i).setBetTypeId(bettypeRepository.findById(requestObject.getBettypeId()));
-        	betRepository.save(bets.get(i));
-        	
-        	betOutcomeMap.add(new BetOutcomeMap());
-        	betOutcomeMap.get(i).setBetId(bets.get(i));
-        	betOutcomeMap.get(i).setOutcomeID(outcomeRepository.findById(currentId));
-        	betOutcomeMap.get(i).setOdds(requestObject.getOutcomeOdds().get(currentId));
-        	betoutcomemapRepository.save(betOutcomeMap.get(i));
-        	
-        	transactions.add(new Transaction());
-        	transactions.get(i).setAccount(accountRepository.findById(requestObject.getAccountId()));
-        	transactions.get(i).setAmount(requestObject.getBetStake().negate());
-        	transactions.get(i).setBet(bets.get(i));        	
-        	transactions.get(i).setDescription("Description.......");
-        	transactions.get(i).setType(TransactionTypes.BET);
-        	transactions.get(i).setCreatedDate(Calendar.getInstance(Locale.getDefault()));
-        	transactionRepository.save(transactions.get(i));
-        	
-        	betResponses.add(BetResponseMapper.mapToResponse(bets.get(i), betRepository, 
-        			transactionRepository, bettypeRepository, 
-        			betoutcomemapRepository, outcomeRepository, 
-        			marketRepository, eventRepository));
-        	betPlacementResponse.setSumStake(betPlacementResponse.getSumStake().add(requestObject.getBetStake()));
-        	betPlacementResponse.setSumPotentialWin(betPlacementResponse.getSumPotentialWin().add(
-        			betResponses.get(betResponses.size()-1).getPotentialWin()));
-        	
+        if(bettypeRepository.findById(requestObject.getBettypeId()).getDescription().equals(BetTypes.Single)) {
+            BetPlacementResponse betPlacementResponse = new BetPlacementResponse();
+            betPlacementResponse.setSumStake(BigDecimal.valueOf(0.));
+            betPlacementResponse.setSumPotentialWin(BigDecimal.valueOf(0.));
+            
+            List <BetResponse> betResponses=new ArrayList <BetResponse>();
+            int NumberOfOutcomes=requestObject.getOutcomeOdds().size();
+            List <Bet> bets=new ArrayList <Bet>();
+            List <Transaction> transactions=new ArrayList <Transaction>();
+            List <BetOutcomeMap> betOutcomeMap=new ArrayList <BetOutcomeMap>();
+            Set <Long> outcomeIds=requestObject.getOutcomeOdds().keySet(); 
+        	Long currentId;
+            
+        	Iterator <Long> iterator=outcomeIds.iterator();
+            for(int i=0;i<NumberOfOutcomes;i++) {
+            	currentId=iterator.next();
+            	bets.add(new Bet());
+            	bets.get(i).setAccountId(accountRepository.findById(requestObject.getAccountId()));
+            	bets.get(i).setBetTypeId(bettypeRepository.findById(requestObject.getBettypeId()));
+            	betRepository.save(bets.get(i));
+            	
+            	betOutcomeMap.add(new BetOutcomeMap());
+            	betOutcomeMap.get(i).setBetId(bets.get(i));
+            	betOutcomeMap.get(i).setOutcomeID(outcomeRepository.findById(currentId));
+            	betOutcomeMap.get(i).setOdds(requestObject.getOutcomeOdds().get(currentId));
+            	betoutcomemapRepository.save(betOutcomeMap.get(i));
+            	
+            	transactions.add(new Transaction());
+            	transactions.get(i).setAccount(accountRepository.findById(requestObject.getAccountId()));
+            	transactions.get(i).setAmount(requestObject.getBetStake().negate());
+            	transactions.get(i).setBet(bets.get(i));      
+            	transactions.get(i).setDescription(bettypeRepository.findById(requestObject.getBettypeId()).getDescription().getValue()+
+            			" Bet "+outcomeRepository.findById(currentId).getDescription()+ " @ "+
+            			outcomeRepository.findById(currentId).getOdds());
+            	transactions.get(i).setType(TransactionTypes.BET);
+            	transactions.get(i).setCreatedDate(Calendar.getInstance(Locale.getDefault()));
+            	transactionRepository.save(transactions.get(i));
+            	
+            	betResponses.add(BetResponseMapper.mapToResponse(bets.get(i), betRepository, 
+            			transactionRepository, bettypeRepository, 
+            			betoutcomemapRepository, outcomeRepository, 
+            			marketRepository, eventRepository));
+            	betPlacementResponse.setSumStake(betPlacementResponse.getSumStake().add(requestObject.getBetStake()));
+            	betPlacementResponse.setSumPotentialWin(betPlacementResponse.getSumPotentialWin().add(
+            			betResponses.get(betResponses.size()-1).getPotentialWin()));
+            	
+            }
+            betPlacementResponse.setListOfBetResponses(betResponses);
+ 
+    		return betPlacementResponse;	
         }
-        betPlacementResponse.setListOfBetResponses(betResponses);
-        
-        
-		return betPlacementResponse;
+        else if(bettypeRepository.findById(requestObject.getBettypeId()).getDescription().equals(BetTypes.Double)) {
+        	return null;
+        }
+        else if(bettypeRepository.findById(requestObject.getBettypeId()).getDescription().equals(BetTypes.Treble)) {
+        	return null;
+        }
+        else return null;
 
     }
 }
