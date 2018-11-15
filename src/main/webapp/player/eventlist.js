2761ec71205e2b8e539f0b7bf42f60ace7bd7df5
dunaroliver <d.oliver95@hotmail.com>
2018-11-15 14:08:39 +0100

    SB-58 Created html and js files for listing events

---
+++ src/main/webapp/player/eventlist.js
@@ -0,0 +1,9 @@
+window.onload = function() {
+    // TODO load account data and balance
+    let player = {
+        username : 'TestPlayer',
+        balance : 12300
+    };
+    SB.Utils.loadTemplate('#navbar-content', 'player_navbar.html', player, null);
+    bindListeners();
+};
