ca455b0798d01a74044ae426427932a30a8382bb
Attila Molnár <mail@molnarattila.net>
2018-11-21 10:50:24 +0100

    SB-57 Competitor create

--- src/main/java/com/tricast/controllers/CompetitorController.java
+++ src/main/java/com/tricast/controllers/CompetitorController.java
@@ -3,11 +3,13 @@ package com.tricast.controllers;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -42,8 +44,8 @@ public class CompetitorController {
 		return competitorManager.findAll();
     }
 
-	@PostMapping
-	public CompetitorResponse create(CompetitorRequest competitorRequest) {
+	@PostMapping(consumes = {MediaType.APPLICATION_JSON_VALUE})
+	public CompetitorResponse create(@RequestBody CompetitorRequest competitorRequest) {
 		return competitorManager.create(competitorRequest);
 	}
 
