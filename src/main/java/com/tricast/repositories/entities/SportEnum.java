2caf6f0cd405869cddd41cc18e9895f98f8573fb
Akos Toth Faber <akos.toth.faber@tricast.hu>
2018-11-21 10:57:50 +0100

    SB-56 add markettypes and period types to sports

--- src/main/java/com/tricast/repositories/entities/SportEnum.java
+++ src/main/java/com/tricast/repositories/entities/SportEnum.java
@@ -1,5 +1,39 @@
 package com.tricast.repositories.entities;
 
+import java.util.Arrays;
+import java.util.List;
+
 public enum SportEnum {
-    FOOTBALL, HORSERACING;
+    FOOTBALL(Arrays.asList(
+                    MarketTypeEnum.WDW,
+                    MarketTypeEnum.CorrectScore,
+                    MarketTypeEnum.HalfTimeFullTime,
+                    MarketTypeEnum.OverUnderCornersEightAndAHalf,
+                    MarketTypeEnum.OverUnderGoalsOneAndAHalf,
+                    MarketTypeEnum.OverUnderGoalsTwoAndAHalf),
+            Arrays.asList(
+                    PeriodTypeEnum.NINETYMINUTES,
+                    PeriodTypeEnum.FIRSTHALF,
+                    PeriodTypeEnum.SECONDHALF)),
+    HORSERACING(Arrays.asList(
+                    MarketTypeEnum.HeadToHead, 
+                    MarketTypeEnum.Outright), 
+           Arrays.asList(
+                    PeriodTypeEnum.RACE));
+
+    private final List<MarketTypeEnum> marketTypes;
+    private final List<PeriodTypeEnum> periodTpes;
+
+    private SportEnum(List<MarketTypeEnum> marketTypes, List<PeriodTypeEnum> periodTpes) {
+        this.marketTypes = marketTypes;
+        this.periodTpes = periodTpes;
+    }
+
+    public List<MarketTypeEnum> getMarketTypes() {
+        return marketTypes;
+    }
+
+    public List<PeriodTypeEnum> getPeriodTpes() {
+        return periodTpes;
+    }
 }
