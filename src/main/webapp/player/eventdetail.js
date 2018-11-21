097b02aef370ab741a4f1d1fabfae3be6998b652
Dunár Olivér <d.oliver95@hotmail.com>
2018-11-21 10:24:47 +0100

    SB-59 EventDetail screen works

--- src/main/webapp/player/eventdetail.js
+++ src/main/webapp/player/eventdetail.js
@@ -7,43 +7,16 @@ window.onload = function() {
     SB.Utils.loadTemplate('#navbar-content', 'player_navbar.html', player, null);
     
     let eventid = localStorage.getItem('eventid');
-    $('#place').html(Handlebars.compile($('#show_event_id').html())({
-        eventid
-    }));
-     
 
-    const destructById = function(array) {
-        let json = {};
-        $.each(array, function(index, elem) {
-            json[elem.id] = elem;
-        });
-        return json;
-    };
 
     let eventsById;
     let url="/sportsbook/api/events/"+eventid+"/detail";
     
-    $.when(SB.Utils.getAjax(url, SB.Token.PLAYER)
-        ).done(
-            function(events) {
-
-                eventsById = destructById(events[0]);
-
-                let eventdetail = [];
-                
-                
-                $.each(eventsById, function(id, event) {
-                    eventdetail.push({
-                        'id' : event.id + '',
-                        'title' : event.description,
-                        'league' : leaguesById[event.leagueId].description,
-                        'startDate' : moment(event.startTime).format('YYYY.MM.DD HH:MM')
-                    });
-                });
-
-                $('#events-table tbody').html(Handlebars.compile($('#event-row-template').html())({
-                    events : eventList
-                }));
+    SB.Utils.getAjax(url, SB.Token.PLAYER,function(events) {
+    	
+   	    $('#eventdetail_table tbody').html(Handlebars.compile($('#show_event_details').html())(
+                   events
+               ));
 
             });
 };
