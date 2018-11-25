38c93231d1ee525163269692a0f76f3a3bf65ad3
Szabados Péter <szabados.peti.96@gmail.com>
2018-11-25 22:58:44 +0100

    SB-49 Trying to decide whether an outcome won or lost.

--- src/main/java/com/tricast/managers/OutcomeManagerImpl.java
+++ src/main/java/com/tricast/managers/OutcomeManagerImpl.java
@@ -1,17 +1,21 @@
 package com.tricast.managers;
 
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.HashSet;
 import java.util.List;
+import java.util.Map;
 import java.util.Set;
 
 import javax.inject.Inject;
 
 import org.springframework.stereotype.Service;
 
+import com.tricast.repositories.CompetitorRepository;
 import com.tricast.repositories.MarketRepository;
 import com.tricast.repositories.OutcomeRepository;
 import com.tricast.repositories.ResultRepository;
+import com.tricast.repositories.entities.Competitor;
 import com.tricast.repositories.entities.Market;
 import com.tricast.repositories.entities.Outcome;
 import com.tricast.repositories.entities.Result;
@@ -22,15 +26,18 @@ public class OutcomeManagerImpl implements OutcomeManager {
     private OutcomeRepository outcomeRepository;
     private ResultRepository resultRepository;
     private MarketRepository marketRepository;
+    private CompetitorRepository competitorRepository;
 
     @Inject
     public OutcomeManagerImpl(
     						OutcomeRepository outcomeRepository,
     						ResultRepository resultRepository,
-    						MarketRepository marketRepository) {
+    						MarketRepository marketRepository,
+							CompetitorRepository competitorRepository) {
         this.outcomeRepository = outcomeRepository;
         this.resultRepository = resultRepository;
         this.marketRepository = marketRepository;
+        this.competitorRepository = competitorRepository;
     }
 
     @Override
@@ -60,11 +67,14 @@ public class OutcomeManagerImpl implements OutcomeManager {
 
 	@Override
 	public Set<Result> findByEventId(long eventId) {
-		List<Outcome> outcomeByEventId = outcomeRepository.findByMarket_Event_Id(eventId);
+		List<Outcome> outcomeByMarketId;
 		List<Result> resultByEventId = resultRepository.findByEventCompetitorMap_Event_Id(eventId);
 		List<Market> marketByEventId = marketRepository.findByEvent_Id(eventId);
-
+		Competitor competitor;
+		
 		Set<Result> resultByEventIdAndMarketType = new HashSet<Result>();
+		Map<Long, String> winnerOutcomeCode = new HashMap<Long, String>();
+		Map<Long, Integer> resultsByOutcome = new HashMap<Long, Integer>();
 		
 		for(Result currentResult : resultByEventId) {
 			for(Market currentMarket : marketByEventId) {
@@ -76,9 +86,22 @@ public class OutcomeManagerImpl implements OutcomeManager {
 				}
 			}
 		}
-
+		
+		for(Market currentMarket : marketByEventId) {
+			outcomeByMarketId = outcomeRepository.findByMarket_Id(currentMarket.getId());
+			for(Outcome currentOutcome : outcomeByMarketId) {
+				for(Result currentResult : resultByEventIdAndMarketType) {
+					competitor = competitorRepository.findById(currentResult.getEventCompetitorMap().getCompetitorId());
+					if(competitor.getDescription().equalsIgnoreCase(currentOutcome.getDescription())) {
+						resultsByOutcome.put(currentOutcome.getId(), currentResult.getResult());
+					}
+				}
+			}
+			for(Map.Entry<Long, Integer> mapValue : resultsByOutcome.entrySet()) {
+			}
+		}
+		
 		
 		return resultByEventIdAndMarketType;
 	}
-
 }
